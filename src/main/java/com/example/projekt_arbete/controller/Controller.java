0aa9fae1133d27921295066d17685839ce3eb14c
mp-104 <mp-104@hotmail.com>
2024-09-11 14:38:19 +0200

    add getFilmById method in Controller

--- src/main/java/com/example/projekt_arbete/controller/Controller.java
+++ src/main/java/com/example/projekt_arbete/controller/Controller.java
@@ -2,6 +2,7 @@ package com.example.projekt_arbete.controller;
 
 import com.example.projekt_arbete.Keys;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -11,18 +12,24 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/films")
 public class Controller {
 
+    String url = String.format("https://api.themoviedb.org/3/movie/550?api_key=%s", Keys.ApiKey);
+    //"https://api.themoviedb.org/3/movie/" + 540 + "?api_key=" + Keys.ApiKey
     private final WebClient webClientConfig;
 
     public Controller (WebClient.Builder webClient) {
         this.webClientConfig = webClient
-                .baseUrl("https://api.themoviedb.org/3/movie/550?api_key=" + Keys.ApiKey)
+                .baseUrl("https://api.themoviedb.org/3/movie")
                 .build();
     }
 
-    @GetMapping
-    public Mono<String> getFilm () {
+    @GetMapping("/{id}")
+    public Mono<String> getFilmById (@PathVariable int id) {
 
         return webClientConfig.get()
+                .uri(film -> film
+                        .path("/" + id)
+                        .queryParam("api_key", Keys.ApiKey)
+                        .build())
                 .retrieve()
                 .bodyToMono(String.class);
     }
